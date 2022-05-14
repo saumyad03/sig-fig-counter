896876f61d5b0289e61f161fca12386b629db81d
saumyad03 <saumyad03@gmail.com>
2022-05-14 13:31:04 -0400

    Update sig-fig-counter.py

--- sig-fig-counter.py
+++ sig-fig-counter.py
@@ -1,22 +1,22 @@
-#sig-fig-counter
 '''
-Rules
-1. Nonzero digits are always significant
-2. Any zeros between two significant digits are significant
-3. A final zero or trailing zeros in the decimal portion 
-ONLY are significant
+PSEUDOCODE (how the if statements are broken down)
+all nonzero are always significant
+
+if no decimal point...
+   zeros significant? --> leading no, trailing no, in between yes
+
+elif decimal point...
+if left is not 0
+   left of decimal point...
+       zeros significant? --> leading no, everything else yes
+   right of decimal point...
+       zeros significant? --> all significant
+elif left is 0
+   left of decimal point...
+       zeros significant? --> no
+   right of decimal point...
+       zeros signficant? --> leading no, everything else yes
 '''
-#all nonzero always significant
-
-#if no decimal point...
-#zeros significant? --> leading no, trailing no, in between yes
-
-#if decimal point...
-#left of decimal point...
-#zeros significant? --> leading no, everything else yes
-#right of decimal point...
-# zeros significant? --> all significant
-
 #function that does the actual checking, parameter is a string
 def checkSigFigs(number):
     #input validation
@@ -43,18 +43,23 @@ def checkSigFigs(number):
         number = number.split('.')
         l = number[0]
         r = number[1]
-        count += len(r)
-        for digit in l:
-            if digit != 0:
-                count += 1
-                if nonZeroSeen == False:
-                    nonZeroSeen = True
-            else:
-                if nonZeroSeen == True:
+        if int(l) != 0:
+            count += len(l) + len(r)
+        else:
+            for digit in r:
+                if digit != '0':
                     count += 1
+                    if nonZeroSeen == False:
+                        nonZeroSeen = True
+                else:
+                    if nonZeroSeen == True:
+                        count += 1
     return count
 
+print("Type quit to stop checking sig figs")
 while True:
     num = input("Enter number to check sig figs for: ")
-    print(checkSigFigs(num))
-
+    if (num == "quit"):
+        break
+    else:
+        print(checkSigFigs(num))
